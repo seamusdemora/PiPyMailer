55739f14a37902cf0238bc41c044aef2e937b1a2
Seamus de Mora <seamusdemora@gmail.com>
2018-04-16 22:45:04 +0100

    new file:   change.crontab.txt	crontab entry to invoke program at reboot new file:   change.profile.txt	changes to ~/.profile to add ENVIRONMENT variables modified:   testmail2.py	modified program as of 04/16/2018 @ 22:30 BST

--- testmail2.py
+++ testmail2.py
@@ -1,29 +1,46 @@
 #!/usr/bin/python3
 import smtplib
 import datetime
+import os
+
+# Create these environment variables in .profile,or another appropriate location
+#export PYMAIL_USER_NAME=""
+#export PYMAIL_USER_PSWD=""
+#export PYMAIL_FWD_ADDR=""
+#export PYMAIL_FRM_ADDR=""
+
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from pytz import reference
 
-fromaddr = "myusername@gmail.com"
-toaddr = "anotherusername@anotherdomain.com"
+# collect values from environment variables
+fromaddr = os.environ.get('PYMAIL_FRM_ADDR')
+toaddr = os.environ.get('PYMAIL_FWD_ADDR')
+userid = os.environ.get('PYMAIL_USER_NAME')
+paswrd = os.environ.get('PYMAIL_USER_PSWD')
+# Get the local time
+localtime = reference.LocalTimezone()
+rightnow = datetime.datetime.now()
+currtz = localtime.tzname(rightnow)
+currtime = str(rightnow)
+# get the hostname from /etc/hostname
+handle = open("/etc/hostname", "r")
+myhostname = handle.readline().rstrip('\n')
+handle.close()
+# Build the message
 thesubject = "Raspberry Pi reboot report"
 msg = MIMEMultipart()
 msg['From'] = fromaddr
 msg['To'] = toaddr
 msg['Subject'] = thesubject
-
-# localtimezone = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
-localtime = reference.LocalTimezone()
-rightnow = datetime.datetime.now()
-currtz = localtime.tzname(rightnow)
-currtime = str(rightnow)
-body = "\nYour Raspberry Pi has experienced a reboot.\n\nThe current time is: " + currtime + " " + currtz
+body = "\n" + myhostname + " has experienced a reboot.\n\nThe current time is: " + currtime + " " + currtz
 msg.attach(MIMEText(body, 'plain'))
-
+# declare SMTP server & open  secure TLS connection
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login(fromaddr, "GMailPassword")
+# login to SMTP server using credentials 
+server.login(userid, paswrd)
+# deliver the mail
 text = msg.as_string()
 server.sendmail(fromaddr, toaddr, text)
 server.quit()
